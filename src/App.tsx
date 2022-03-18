a254655684afd646505eee2eab1140465cebd501
Parthiban Baskar <parthi.krb@gmail.com>
2022-03-18 11:41:07 +0100

    chore: prevent commit on main branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.tsx to start checking on your app!</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
