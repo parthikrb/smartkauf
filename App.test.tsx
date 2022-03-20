68ec7ab59974d908da9beedb66f8f267f08c29e8
Parthiban Baskar <parthi.krb@gmail.com>
2022-03-20 22:32:48 +0100

    test: add inline snapshot

--- App.test.tsx
+++ App.test.tsx
@@ -8,4 +8,26 @@ describe('<App />', () => {
     const tree: ReactTestRendererJSON = renderer.create(<App />).toJSON() as ReactTestRendererJSON;
     expect(tree?.children?.length).toBe(1);
   });
+
+  it('displays correctly', () => {
+    const tree: ReactTestRendererJSON = renderer.create(<App />).toJSON() as ReactTestRendererJSON;
+    expect(tree).toMatchInlineSnapshot(
+      `
+      <View
+        style={
+          Object {
+            "alignItems": "center",
+            "backgroundColor": "#fff",
+            "flex": 1,
+            "justifyContent": "center",
+          }
+        }
+      >
+        <Text>
+          Open up App.tsx to start checking on your app!
+        </Text>
+      </View>
+    `,
+    );
+  });
 });
