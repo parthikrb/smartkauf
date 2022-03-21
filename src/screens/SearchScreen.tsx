a445e86e846cdb174cff1a67d2b95e0e8f8a4ece
Parthiban Baskar <parthi.krb@gmail.com>
2022-03-21 11:27:40 +0100

    add screens

---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import GlobalStyles from '../GlobalStyles';
+
+const SearchScreen = () => {
+  return (
+    <View style={GlobalStyles.screenContainer}>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/StoreScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import GlobalStyles from '../GlobalStyles';
+
+const StoreScreen = () => {
+  return (
+    <View style={GlobalStyles.screenContainer}>
+      <Text>StoreScreen</Text>
+    </View>
+  );
+};
+
+export default StoreScreen;
+
+const styles = StyleSheet.create({});
