9bd2a2209e7ecadf4de734cf1d1fe6e69bdb4502
Parthiban Baskar <parthi.krb@gmail.com>
2022-03-22 17:59:16 +0100

    test: update the snapshot

--- App.spec.tsx
+++ App.spec.tsx
@@ -96,6 +96,39 @@ describe('<App />', () => {
                     <Text>
                       StoreScreen
                     </Text>
+                    <View
+                      accessible={true}
+                      collapsable={false}
+                      focusable={true}
+                      onClick={[Function]}
+                      onResponderGrant={[Function]}
+                      onResponderMove={[Function]}
+                      onResponderRelease={[Function]}
+                      onResponderTerminate={[Function]}
+                      onResponderTerminationRequest={[Function]}
+                      onStartShouldSetResponder={[Function]}
+                      style={
+                        Object {
+                          "alignItems": "center",
+                          "backgroundColor": "#48A14D",
+                          "borderRadius": 25,
+                          "bottom": 90,
+                          "display": "flex",
+                          "height": 50,
+                          "justifyContent": "center",
+                          "opacity": 1,
+                          "position": "absolute",
+                          "right": 20,
+                          "width": 50,
+                        }
+                      }
+                    >
+                      <View
+                        color="#F3F9FC"
+                        name="add-outline"
+                        size={40}
+                      />
+                    </View>
                   </View>
                 </View>
               </View>
@@ -373,7 +406,7 @@ describe('<App />', () => {
                   >
                     <View
                       color="#48A14D"
-                      name="search-circle"
+                      name="search"
                       size={25}
                     />
                   </View>
@@ -397,7 +430,7 @@ describe('<App />', () => {
                   >
                     <View
                       color="#828382"
-                      name="search-circle-outline"
+                      name="search-outline"
                       size={25}
                     />
                   </View>
