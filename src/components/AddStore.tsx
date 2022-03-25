1449fd515173091479fc22c43190ff859d65be75
Parthiban Baskar <parthi.krb@gmail.com>
2022-03-25 13:43:15 +0100

    feat: add location

--- src/components/AddStore.tsx
+++ src/components/AddStore.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BottomSheet } from 'react-native-btr';
+import * as Location from 'expo-location';
 import colors from '../config/colors';
 
 type Props = {
@@ -9,10 +10,46 @@ type Props = {
 };
 
 const AddStore = ({ visible, toggle }: Props) => {
+  const [location, setLocation] = useState<string>('Getting Location...');
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    void (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Highest,
+        timeInterval: 5000,
+      });
+
+      const [position] = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      const address = `${position.street as string} ${position.streetNumber as string}, ${
+        position.district as string
+      }, ${position.region as string} - ${position.postalCode as string}`;
+      setLocation(address);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (errorMsg) {
+      setLocation(errorMsg);
+    }
+  }, [errorMsg, location]);
+
   return (
     <BottomSheet visible={visible} onBackButtonPress={toggle} onBackdropPress={toggle}>
       <View style={styles.container}>
-        <Text>Add Store</Text>
+        <Text>{location}</Text>
       </View>
     </BottomSheet>
   );
