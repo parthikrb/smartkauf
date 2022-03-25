1aeb37cab7563fd9d06c0e7c8218189fe4831a64
Parthiban Baskar <parthi.krb@gmail.com>
2022-03-25 14:23:43 +0100

    feat: complete add store design

--- src/components/AddStore.tsx
+++ src/components/AddStore.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { BottomSheet } from 'react-native-btr';
+import { Ionicons } from '@expo/vector-icons';
 import * as Location from 'expo-location';
 import colors from '../config/colors';
 
@@ -49,7 +50,16 @@ const AddStore = ({ visible, toggle }: Props) => {
   return (
     <BottomSheet visible={visible} onBackButtonPress={toggle} onBackdropPress={toggle}>
       <View style={styles.container}>
-        <Text>{location}</Text>
+        <Text style={styles.title}>New Store</Text>
+
+        <TextInput style={styles.input} placeholder="Store Name" />
+        <View style={styles.locationContainer}>
+          <Ionicons name="location-outline" size={16} color={colors.black} />
+          <Text style={styles.location}>{location}</Text>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={toggle}>
+          <Text style={styles.buttonText}>Add Store</Text>
+        </TouchableOpacity>
       </View>
     </BottomSheet>
   );
@@ -65,8 +75,49 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.white,
     width: '100%',
-    height: 400,
+    height: 280,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  title: {
+    color: colors.black,
+    fontSize: 32,
+    fontWeight: 'bold',
+    position: 'absolute',
+    top: 20,
+  },
+  input: {
+    borderBottomWidth: 0.8,
+    borderColor: colors.neutral,
+    width: '80%',
+    height: 40,
+    fontSize: 24,
+    padding: 5,
+    paddingBottom: 0,
+    marginBottom: 30,
+  },
+  locationContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '80%',
+    flexWrap: 'wrap',
+  },
+  location: {
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: colors.primary,
+    width: '80%',
+    height: 50,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 10,
+    marginVertical: 30,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
   },
 });
